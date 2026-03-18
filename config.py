b999e8ac07ea3a417414eb3fbb3f9b6158dd1a28
Siim Ilison <ilison@ut.ee>
2026-03-18 22:23:56 +0200

    Created separate file config.py for all the database info and thus updated database.py file

---
+++ config.py
@@ -0,0 +1,4 @@
+DB_HOST = "localhost"
+DB_NAME = "employee_projects_db"
+DB_USER = "postgres"
+DB_PASSWORD = "postgres"
--- database.py
+++ database.py
@@ -1,13 +1,14 @@
 import psycopg2
 from psycopg2.extras import RealDictCursor
+import config
 
 # CONNECTION SETUP
 def get_connection():
     return psycopg2.connect(
-        host="localhost",
-        database="employee_projects_db",
-        user="postgres",
-        password="postgres" 
+        host=config.DB_HOST,
+        database=config.DB_NAME,
+        user=config.DB_USER,
+        password=config.DB_PASSWORD 
     )
 
 
