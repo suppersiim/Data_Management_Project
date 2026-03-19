ae3bc2cada3c7eb944e09d6f88b96d3be2cce7e9
Siim Ilison <ilison@ut.ee>
2026-03-19 20:10:22 +0200

    Modified config.py file

--- config.py
+++ config.py
@@ -1,4 +1,11 @@
-DB_HOST = "localhost"
-DB_NAME = "employee_projects_db"
-DB_USER = "postgres"
-DB_PASSWORD = "postgres"
+import os
+from dotenv import load_dotenv
+
+# Only load .env file if NOT running inside Docker
+if not os.environ.get("DB_HOST"):
+    load_dotenv()
+
+DB_HOST     = os.environ.get("DB_HOST", "localhost")
+DB_NAME     = os.environ.get("DB_NAME", "employee_projects_db")
+DB_USER     = os.environ.get("DB_USER", "postgres")
+DB_PASSWORD = os.environ.get("DB_PASSWORD", "postgres")
